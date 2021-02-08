bc907f6bfdafdee975bb4a145783514a21df7516
JSins <nutzlosundso@gmail.com>
2021-02-08 22:29:30 +0100

    ein paar änderungen, einstellungen hinzugefügt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,7 @@ import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
 
+import "./src/custom.css"
+
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import contactInfo from "../../site/settings/contact_info.json"
+
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
@@ -14,6 +16,7 @@ const BlogIndex = ({ data, location }) => {
       <Layout location={location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
+
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
@@ -27,6 +30,8 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
+      {contactInfo.phone}
+
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
